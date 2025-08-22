4ae41e7d3719130b4f0212513475c8d5209d0488
1f1024yamaguchi <s1f102401472@iniad.org>
2025-08-22 19:33:22 +0900

    時間経過とともに敵が速くなる

--- Assets/Jinnyama/Scripts/EnemyMove.cs
+++ Assets/Jinnyama/Scripts/EnemyMove.cs
@@ -6,16 +6,29 @@ public class EnemyMove : MonoBehaviour
 {
     [SerializeField] private FirstPersonController _playerController;
     private NavMeshAgent _agent;
+    [Header("速度上昇設定")]
+    [SerializeField] private float speedUpInterval = 10f; //速度が上がる間隔
+    [SerializeField] private float speedIncrement = 0.5f; //一度に増幅する速度
+    private float timer; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>(); //navmeshagentを保持しておく
+        timer = 0f;//タイマー初期化
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        timer += Time.deltaTime;
+        if(timer >speedUpInterval)
+        {
+            _agent.speed += speedIncrement;
+            Debug.Log("速度上昇" + _agent.speed);
+            timer = 0f; //タイマーリセット
+        }
         _agent.destination = _playerController.transform.position;
         //プレイヤーを目指して進む
         
---
+++ Assets/Jinnyama/Scripts/EnemyStatus.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+
+public class EnemyStatus : MonoBehaviour
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
