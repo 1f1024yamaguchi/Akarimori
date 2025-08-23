cac24dff0910d30cf320ba4361f3e07c1927be97
1f1024yamaguchi <s1f102401472@iniad.org>
2025-08-24 03:49:49 +0900

    スタートプログラム、テキストの追加

---
+++ Assets/Jinnyama/Scripts/StartBotton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class StartBotton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("MainScene");
+        });
+        
+    }
+
+    
+}
