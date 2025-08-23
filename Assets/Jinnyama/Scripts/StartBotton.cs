688393c6d41097ab63fa557f3fd1c81eb08367ff
1f1024yamaguchi <s1f102401472@iniad.org>
2025-08-24 04:06:24 +0900

    スタートボタンの実装と場面転換スクリプトの修正

--- Assets/Jinnyama/Scripts/StartBotton.cs
+++ Assets/Jinnyama/Scripts/StartBotton.cs
@@ -11,7 +11,7 @@ public class StartBotton : MonoBehaviour
         var button = GetComponent<Button>();
         button.onClick.AddListener(() =>
         {
-            SceneManager.LoadScene("MainScene");
+            SceneManager.LoadScene("Main_Scene");
         });
         
     }
