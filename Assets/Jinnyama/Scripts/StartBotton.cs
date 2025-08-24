3e81bb28896fb9bbf527912ec70249d7c20f5e61
1f1024yamaguchi <s1f102401472@iniad.org>
2025-08-25 04:52:58 +0900

    bgm追加、クリック時に音が鳴る

--- Assets/Jinnyama/Scripts/StartBotton.cs
+++ Assets/Jinnyama/Scripts/StartBotton.cs
@@ -1,19 +1,40 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
 
-[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(Button), typeof(AudioSource))]
 public class StartBotton : MonoBehaviour
 {
+    [Tooltip("クリック時に再生するサウンド")]
+    public AudioClip clickSound;
+    private AudioSource audioSource; //音を再生するためのコンポーネント
+    private Button button;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        var button = GetComponent<Button>();
-        button.onClick.AddListener(() =>
-        {
-            SceneManager.LoadScene("Main_Scene");
-        });
+        audioSource = GetComponent<AudioSource>();
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(HandleClick);
         
+    }
+
+    private void HandleClick()
+    {
+        StartCoroutine(LoadSceneAfterSound());
+    }
+
+    private IEnumerator LoadSceneAfterSound()
+    {
+        button.interactable = false;
+
+        audioSource.PlayOneShot(clickSound);
+
+        //一秒待つ
+        yield return new WaitForSeconds(1.0f);
+
+        SceneManager.LoadScene("Main_Scene");
     }
 
     
