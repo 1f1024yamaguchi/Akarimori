4c7074b88c0ef50483a61dc7239bca03ecac7bf3
1f1024yamaguchi <s1f102401472@iniad.org>
2025-08-25 19:52:37 +0900

    敵が近づいた時のポストプロセスとse

--- Assets/Jinnyama/Scripts/GameOver_to_home.cs
+++ Assets/Jinnyama/Scripts/GameOver_to_home.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class GameOver_to_home : MonoBehaviour
 {
+
+    public float delayInSeconds = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(LoadSceneAfterDelay());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator LoadSceneAfterDelay()
     {
-        
+        //指定された時間だけ待つ
+        yield return new WaitForSeconds(delayInSeconds);
+
+        SceneManager.LoadScene("Start_Scene");
+
     }
 }
